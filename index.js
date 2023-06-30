f0723666729dc0e5278495dafca64b0026387b90
ragulhpSPK <ragulhp27@gmail.com>
2023-06-30 15:02:00 +0530

    gfg

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-app-lemon-eight.vercel.app/",
+    origin: ["https://chat-app-lemon-eight.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
